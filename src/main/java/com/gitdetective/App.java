22da9ac454fcb43ec784942bc696d6adb73a2357
Brandon Fergerson <fergerson92@gmail.com>
2018-04-04 22:55:55 -0400

    initial commit

---
+++ src/main/java/com/gitdetective/App.java
@@ -0,0 +1,13 @@
+package com.gitdetective;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        MyClass yay = new MyClass();
+        yay.myMethod();
+    }
+}
