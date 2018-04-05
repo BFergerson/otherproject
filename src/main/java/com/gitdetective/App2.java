fdc906c056f0b43628d4360fd37edcf557e3427f
Brandon Fergerson <fergerson92@gmail.com>
2018-04-05 11:11:25 -0400

    Update and rename App.java to App2.java

--- src/main/java/com/gitdetective/App2.java
+++ src/main/java/com/gitdetective/App2.java
@@ -3,7 +3,7 @@ package com.gitdetective;
 /**
  * Hello world!
  */
-public class App {
+public class App2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
