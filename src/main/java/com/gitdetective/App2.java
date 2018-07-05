85f8fe10f0d701c5e6253cc98b56239584b7877c
Brandon Fergerson <fergerson92@gmail.com>
2018-07-05 17:16:51 -0400

    test

--- src/main/java/com/gitdetective/App2.java
+++ src/main/java/com/gitdetective/App2.java
@@ -1,6 +1,9 @@
 package com.gitdetective;
 
 import com.google.common.collect.Lists;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.List;
 
 /**
@@ -10,6 +13,19 @@ public class App2 {
     static {
         new MyClass().myMethod();
         System.out.println(String.valueOf(10));
+    }
+    static {
+        final int defaultValue = 64;
+        int value = defaultValue;
+        try {
+            value = Integer.parseInt(AccessController.doPrivileged(new PrivilegedAction<String>() {
+                public String run() {
+                    return System.getProperty("test", String.valueOf(defaultValue));
+                }
+            }));
+        } catch (Exception ignored) {
+        }
+        System.out.println(value);
     }
 
     public static void main(String[] args) {
@@ -22,7 +38,7 @@ public class App2 {
         yay.myMethod();
         System.out.println(arrayList.size());
     }
-    
+
     public static void anotherOne() {
         MyClass yay = new MyClass();
         yay.myMethod();
